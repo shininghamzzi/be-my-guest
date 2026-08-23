7f1384193abca2c21dba5add064a63a8d4fc4fdc
hamzzi <shininghamzzi@gmail.com>
2026-08-24 02:21:27 +0900

    chore: 리다이렉트 코드 추가

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/tab",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
